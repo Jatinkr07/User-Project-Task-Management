e22b2fde03a0174ec394920f6e432a8bf70894e0
Jatinkr07 <jatinkumar07911@gmail.com>
2025-05-01 11:12:30 +0530

    Fixing CORS issue

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3003",
+    origin: "https://user-project-task-management.onrender.com",
     credentials: true,
   })
 );
