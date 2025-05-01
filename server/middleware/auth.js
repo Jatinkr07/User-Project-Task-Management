85925d771d1f4cc0e08ccb6c977e58d68fe28eb8
Jatinkr07 <jatinkumar07911@gmail.com>
2025-05-01 11:57:43 +0530

    changes in backend jwt auth fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,27 +1,25 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const auth = async (req, res, next) => {
-  console.log("Token------>", req.cookies.token);
-  console.log("Token------>", token);
   try {
     const token =
       req.cookies.token ||
       (req.header("Authorization")
         ? req.header("Authorization").replace("Bearer ", "")
         : null);
-    console.log("Token2------>", token);
+
+    console.log("Token------>", token); 
 
     if (!token) {
       return res.status(401).json({ message: "Authentication required" });
     }
-
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Find user
+  
     const user = await User.findById(decoded.userId).select("-password");
-
     if (!user) {
       return res.status(401).json({ message: "User not found" });
     }
