2eedd7573d3e0aad130aae426e207350cc061b79
Jatinkr07 <jatinkumar07911@gmail.com>
2025-05-02 15:07:59 +0530

    changes in Backend middeware done

--- server/index.js
+++ server/index.js
@@ -17,8 +17,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://user-project-task-management.onrender.com", "http://localhost:5173"],
+    origin: [
+      "https://user-project-task-management.onrender.com",
+      "http://localhost:5173",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
